a5e947c4599f48a408057c372dd289cb90a018af
Sergii.Pushkovskyi <Sergii.Pushkovskyi@infopulse.com>
2017-11-20 11:29:25 +0200

    Completed home work, made some improvements

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,6 +6,27 @@ public class MainClass {
 
         f.printFibonacciNumbers(21);
 
+        System.out.println("---------------//-----------");
+
+
+
+        Person bill = new Person("Bill", 28, "male");
+        Person john = new Person("John", 31, "male");
+
+
+        System.out.println(john);
+        System.out.println(bill);
+
+
+        System.out.println(bill.hashCode());
+        System.out.println(john.hashCode());
+
+        System.out.println(john.equals(bill));
+
+        String string = "Test string";
+        System.out.println(string.equals("Test string"));
+
+
 
 
 
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
