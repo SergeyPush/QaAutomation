bfaa803f6e25439d85181f9becf2dfe40256e145
Sergii.Pushkovskyi <Sergii.Pushkovskyi@infopulse.com>
2017-11-28 12:46:16 +0200

    Fixed fibonacci numbers homework

--- src/Lesson2/Fibonacci.java
+++ src/Lesson2/Fibonacci.java
@@ -6,13 +6,11 @@ class Fibonacci {
     private int a = 0;
     private int b = 1;
 
-
     void printFibonacciNumbers(int number) {
 
         System.out.println("Lesson2.Fibonacci numbers:");
 
-        for (int c = 0; number > 0; number--) {
-
+        for (int c = 1; number > 0; number--) {
 
             System.out.print(c + " ");
             c = a + b;
